a7c8882b6fbc0cdb1afb300d4ee530e4fcad1fa7
Peter Ivanics <peter.ivanics@reslink.fi>
2016-12-05 23:22:55 +0200

    Installed Cocoapods

---
+++ Pods/Target Support Files/Pods-ScratchOffMapUITests/Pods-ScratchOffMapUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_ScratchOffMapUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ScratchOffMapUITestsVersionString[];
+
