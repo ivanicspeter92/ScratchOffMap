1c442aea50412f41fb07291c124bf8879f940be8
Peter Ivanics <peter.ivanics@reslink.fi>
2016-12-06 22:55:17 +0200

    Added Country.json and Country.swift

---
+++ ScratchOffMap/Models/Country.swift
@@ -0,0 +1,14 @@
+//
+//  Country.swift
+//  ScratchOffMap
+//
+//  Created by Peter Ivanics on 06/12/2016.
+//  Copyright © 2016 Peter Ivanics. All rights reserved.
+//
+
+import UIKit
+
+struct Country {
+    let name: String
+    let code: String
+}
