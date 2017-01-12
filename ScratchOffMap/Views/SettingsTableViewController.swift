d6f066b3a47033cbfe3c2d49b8ee2aac0b459aa3
Peter Ivanics <peter.ivanics@reslink.fi>
2017-01-12 15:45:42 +0200

    Added UI feature to delete points

--- ScratchOffMap/Views/SettingsTableViewController.swift
+++ ScratchOffMap/Views/SettingsTableViewController.swift
@@ -28,6 +28,8 @@ class SettingsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.section == 0 && indexPath.row == 0 {
             self.displayDutyCyclingPicker()
+        } else if indexPath.section == 0 && indexPath.row == 2 {
+            self.displayDeletePointsConfirmation()
         }
     }
     
@@ -41,7 +43,7 @@ class SettingsTableViewController: UITableViewController {
     // MARK: - Private functions
     
     private func displayDutyCyclingPicker() {
-        let alert = UIAlertController(title: "Duty Cycling", message: nil, preferredStyle: .actionSheet)
+        let alert = UIAlertController(title: "Duty Cycling", message: nil, preferredStyle: .alert)
         
         alert.addAction(UIAlertAction(title: "5 minutes", style: .default, handler: { (UIAlertAction) in
             
@@ -58,6 +60,18 @@ class SettingsTableViewController: UITableViewController {
         alert.addAction(UIAlertAction(title: "Custom", style: .default, handler: { (UIAlertAction) in
             
         }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func displayDeletePointsConfirmation() {
+        let alert = UIAlertController(title: "Delete Points", message: "Are you sure you want to delete all colelcted points?", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { (UIAlertAction) in
+            
+        }))
+        
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         
         self.present(alert, animated: true, completion: nil)
