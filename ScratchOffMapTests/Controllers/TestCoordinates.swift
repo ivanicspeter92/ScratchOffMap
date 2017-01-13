b68d68852c6325dc5a74a8c4d088265919aabccb
Peter Ivanics <peter.ivanics@reslink.fi>
2017-01-13 11:14:17 +0200

    Fixed failing tests due to invalidated objects

--- ScratchOffMapTests/Controllers/TestCoordinates.swift
+++ ScratchOffMapTests/Controllers/TestCoordinates.swift
@@ -9,13 +9,19 @@
 @testable import ScratchOffMap
 
 class TestCoordinates {
-    static let kumpula = Coordinates(latitude: 60.208445, longitude: 24.966967)
-    static let pointsInHungary = [
-        Coordinates(latitude: 47.4979, longitude: 19.0402),
-        Coordinates(latitude: 47.361515, longitude: 19.852087)
-    ]
-    static let pointsNotInAnyCountry = [
-        Coordinates(latitude: 28.55943, longitude: -140.11963),
-        Coordinates(latitude: -64.04144, longitude: -155.58838)
-    ]
+    static var kumpula: Coordinates {
+        return Coordinates(latitude: 60.208445, longitude: 24.966967)
+    }
+    static var pointsInHungary: [Coordinates] {
+        return [
+            Coordinates(latitude: 47.4979, longitude: 19.0402),
+            Coordinates(latitude: 47.361515, longitude: 19.852087)
+        ]
+    }
+    static var pointsNotInAnyCountry: [Coordinates] {
+        return [
+            Coordinates(latitude: 28.55943, longitude: -140.11963),
+            Coordinates(latitude: -64.04144, longitude: -155.58838)
+        ]
+    }
 }
